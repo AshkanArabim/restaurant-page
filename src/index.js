9f3ac82e6d2b52a67169db2bc06e4e48cd523271
AshkanArabim <ashkan.arabim@gmail.com>
2022-08-14 16:57:01 -0600

    compile production mode

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ import './pages/about';
 import './events';
 import './reset.css';
 import './style.css';
+
+const makeCredits = (function() {
+    const body = document.querySelector('body');
+    const credits = document.createElement('span');
+    const cr1 = document.createElement('p');
+
+    credits.id = 'credits';
+    cr1.textContent = 'Background image by Emre Can Acer | Menu content from 2Ten Coffee Roasters | © AshkanArabim';
+
+    credits.appendChild(cr1);
+    body.appendChild(credits);
+}) ();
